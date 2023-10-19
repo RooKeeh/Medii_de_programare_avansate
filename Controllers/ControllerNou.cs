b26c56ba65b1a58cca91829616d40911981ea058
Andrei Moldovan <andreimoldovan6@gmail.com>
2023-10-19 18:24:02 +0300

    End Lab1

---
+++ Controllers/ControllerNou.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Moldovan_Andrei_Lab1.Controllers
+{
+    public class ControllerNou : Controller
+    {
+        public string Index()
+        {
+            return "Welcome User!";
+        }
+
+        public string Hello()
+        {
+            return "Hello User!";
+        }
+
+        public string Parameters(string a, int b)
+        {
+            a = "Mesaj";
+            b = 1;
+            return (a + ' ' + b);
+        }
+    }
+}
