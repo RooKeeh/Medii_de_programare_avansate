99f6b8c8f9df99b20d55f0e769f8463b65054e32
Andrei Moldovan <andreimoldovan6@gmail.com>
2023-10-19 19:38:39 +0300

    End Lab 2

---
+++ Data/LibraryContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moldovan_Andrei_Lab1.Models;
+
+namespace Moldovan_Andrei_Lab1.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().ToTable("Customer");
+            modelBuilder.Entity<Order>().ToTable("Order");
+            modelBuilder.Entity<Book>().ToTable("Book");
+        }
+    }
+}
